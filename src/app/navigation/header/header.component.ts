6da5137f44948850603e698703d5be28ef43a94a
bbaral <bbaral@asu.edu>
2018-09-26 01:07:46 -0400

    tslint - kebab-case shit

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Subscription} from 'rxjs';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
